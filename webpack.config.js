ea4192ae2ca8f581a71a2fd3ad054ba53c4165bc
Seth Goldman <seth.daniel.goldman@gmail.com>
2016-10-24 12:12:50 -0400

    Sets up new app from scratch. Builds out app folder, and webpack file.

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.export = {
+	entry: [
+		'./app/index.js'
+	],
+	output: {
+		path: __dirname + '/dist',
+		filename: "index_bundle.js"
+	},
+	module: {
+		loaders: [
+			{test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
+		]
+	}
+}
